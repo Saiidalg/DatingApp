8dbadab6d549f193971b9d53c87a271c3a1cde2b
saiid <saiidalg@hotmail.com>
2021-12-20 00:14:25 +0200

    update

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,13 +12,13 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accoutService: AccountService) { }
+  constructor(public accoutService: AccountService,private router: Router) { }
 
   ngOnInit(): void {
   }
   login(){
     this.accoutService.login(this.model).subscribe(Response => {
-      console.log(Response);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
     
@@ -26,6 +27,7 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accoutService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
